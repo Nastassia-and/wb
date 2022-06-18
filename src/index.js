a039cfeb3d364b1fcb4927f716866754df00db8a
kmdmitrievich <kmdmitrievich@gmail.com>
2022-06-18 16:17:32 +0300

    Basket

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import result from './components/cards/createcards';
+import { openBasket } from './components/basket/basket';
+
+document.getElementById('basket').addEventListener('click', openBasket);
