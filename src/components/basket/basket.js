22ecd08abbb3a2cc5be960a873e14996dd5f4101
kmdmitrievich <kmdmitrievich@gmail.com>
2022-06-19 22:49:03 +0300

    Fix bascet and cards

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -9,16 +9,31 @@ export function openBasket() {
     arr.forEach(element => {
         const basketElement = document.createElement('div');
         basketElement.classList.add("basket");
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Esc";
+        deleteButton.classList.add('deleteButton');
+
+        deleteButton.addEventListener('click', () => {
+            basketElement.remove();
+            // localStorage.removeItem('basket');  ХЗ, как удалить из local определенный элемент
+        });
+
         const spanName = document.createElement("span");
-        spanName.classList.add("span1");
+        spanName.classList.add("span");
         spanName.textContent = element.name;
-        basketElement.append(spanName);
 
         const spanPrice = document.createElement("span");
-        spanPrice.textContent = element.price;
-        basketElement.append(spanPrice);
+        spanPrice.textContent = (`${element.price} руб.`);
 
+        basketElement.prepend(deleteButton, spanName, spanPrice);
         containerBasket.append(basketElement);
+
+        document.getElementsByClassName('btn-secondary')[0].addEventListener('click', () => {
+            localStorage.removeItem('basket');
+            document.getElementsByClassName('container-baskets')[0].innerHTML = 'Корзина пуста';
+            document.getElementById('sum').innerHTML = '0 руб.';
+        });
     });
 
     getSum();
@@ -30,6 +45,5 @@ function getSum() {
     if (localStorage.getItem('basket')) {
         arr = JSON.parse(localStorage.getItem('basket'));
     }
-
-    document.getElementById('sum').textContent = arr.reduce((sum, item) => sum += item.price, 0);
+    document.getElementById('sum').textContent = (`${arr.reduce((sum, item) => sum += item.price, 0)} руб.`);
 }
